bdfa8454c927f277c37edfd717d1e1d6a0aab245
Leo <leonardthiele@googlemail.com>
2018-05-02 01:44:32 +0200

    add comment to js

--- assets/js/super-important.js
+++ assets/js/super-important.js
@@ -1,2 +1,4 @@
+// Stop! Dont' you think this is a little too far?
+
 console.log("%c+","font-size: 1px; padding: 0px 180px; line-height: 180px; background: url(https://i.imgur.com/uWtb7Fk.gif); background-size: 320px 180px; background-repeat:no-repeat; color: transparent;");
 console.log('%c Hey, what are you doing here?', "font-size: 20px;");
